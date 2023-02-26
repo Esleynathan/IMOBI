3d169ffda9c7d187f7aabc1266812cdf089d7b17
Esley <esleynathan@hotmail.com>
2023-02-26 14:48:39 -0300

    IMOBI | Logout (Url e Views), APP plataforma, Home(Url,View), Tabelas.

---
+++ plataforma/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/auth/logar')
+def home(request):
+    imoveis = Imovei.objects.all()
+    return render(request, 'home.html', {'imoveis': imoveis})
